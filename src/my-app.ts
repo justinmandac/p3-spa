f3f5fab4768a08dbcfb37571297d7510d644d6ac
justinmandac <justinmandac@gmail.com>
2018-07-04 00:17:49 +0800

    Initial commit

---
+++ src/my-app.ts
@@ -0,0 +1,15 @@
+import {
+  PolymerElement,
+  html,
+} from '@polymer/polymer/polymer-element';
+import template from './my-app.html';
+
+class MyApp extends PolymerElement {
+  static get is() { return 'my-app'; }
+
+  static get template() {
+    return html([`${template}`]);
+  }
+}
+
+customElements.define(MyApp.is, MyApp);

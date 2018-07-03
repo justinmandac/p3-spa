3d3a5f5844bba3a2f7902fdc4f7819b7cb0c3fb7
justinmandac <justinmandac@gmail.com>
2018-07-04 00:13:45 +0800

    Moved my-app template to separate html file

--- src/my-app.js
+++ src/my-app.js
@@ -3,21 +3,13 @@ import {
   html,
 } from '@polymer/polymer/polymer-element';
 import './my-header/my-header';
+import template from './my-app.html';
 
 class MyApp extends PolymerElement {
   static get is() { return 'my-app'; }
 
   static get template() {
-    return html`
-      <style>
-        :host {
-          display: block;
-          margin: 0;
-          padding: 0;
-        }
-      </style>
-      <my-header title="My App"></my-header>
-    `;
+    return html([`${template}`]);
   }
 }
 
